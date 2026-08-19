173017f070e2bc3a16c474670c78adc593d6ce75
Barkat-Ullah <barkatullah585464@gmail.com>
2026-08-19 10:50:48 +0600

    pricing : add a pricing section

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -3,6 +3,8 @@
 import Feature from "./components/Featured";
 import HowItWorks from "./components/HowItWorks";
 import Metrics from "./components/Metrix";
+import Pricing from "./components/PriceSection";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
       <Metrics />
       <Feature />
       <HowItWorks />
+      <WhyChooseUs />
+      <Pricing />
     </>
   );
 };
